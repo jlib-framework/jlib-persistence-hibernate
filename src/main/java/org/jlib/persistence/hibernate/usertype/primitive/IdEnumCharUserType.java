16d9b04adb39814fecbe7a9d7d7f6efb5c5b8a4d
Igor Akkerman <igor.akkerman@dynamite-services.de>
2015-02-24 12:24:14 +0100

    jlib-reflect module created, files moved, usages adapted as far as possible

--- src/main/java/org/jlib/persistence/hibernate/usertype/primitive/IdEnumCharUserType.java
+++ src/main/java/org/jlib/persistence/hibernate/usertype/primitive/IdEnumCharUserType.java
@@ -8,21 +8,20 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-import org.jlib.core.classinstance.ClassInstanceException;
-import org.jlib.core.classinstance.InvalidMethodException;
-import org.jlib.core.classinstance.WrongTypedInstanceException;
-
 import org.hibernate.HibernateException;
 import static org.hibernate.type.StandardBasicTypes.CHARACTER;
 import org.hibernate.usertype.ParameterizedType;
-import static org.jlib.core.reflection.ReflectionUtility.findClass;
-import static org.jlib.core.reflection.ReflectionUtility.invokeStaticMethod;
 import org.jlib.persistence.hibernate.usertype.ImmutableOptionalSingleColumnUserType;
 import org.jlib.persistence.hibernate.usertype.InvalidUserTypeParameterValueException;
 import org.jlib.persistence.hibernate.usertype.InvalidUserTypeParametersException;
 import static org.jlib.persistence.hibernate.usertype.UserTypeUtility.assertValidParametersCount;
 import static org.jlib.persistence.hibernate.usertype.UserTypeUtility.getParameterValue;
 import org.jlib.persistence.jpa.IdEnum;
+import org.jlib.reflect.ClassInstanceException;
+import org.jlib.reflect.InvalidMethodException;
+import org.jlib.reflect.InvalidValueException;
+import static org.jlib.reflect.Reflectors.useClass;
+import org.jlib.reflect.WrongTypedInstanceException;
 
 public class IdEnumCharUserType<EnumValue extends Enum<EnumValue> & IdEnum<Id>, Id extends Serializable>
 extends ImmutableOptionalSingleColumnUserType<EnumValue>
@@ -49,10 +48,15 @@ implements ParameterizedType {
     protected EnumValue get(final ResultSet resultSet, final String columnName)
     throws HibernateException, SQLException {
         try {
+            // TODO: verify correct use
             final Object enumValueId = resultSet.getObject(columnName);
-            return invokeStaticMethod(enumClass, enumValueIdentifierMethodName, enumClass, enumValueId);
+            return useClass(enumClass).useStaticMethod(enumValueIdentifierMethodName)
+                                      .withReturnType(enumClass)
+                                      .withArgumentTypes(Object.class)
+                                      .invoke(enumValueId)
+                                      .get();
         }
-        catch (final InvalidMethodException | WrongTypedInstanceException exception) {
+        catch (final InvalidValueException | InvalidMethodException | WrongTypedInstanceException exception) {
             throw new InvalidUserTypeParameterValueException(parameters, PARAMETERNAME_ENUM_VALUE_METHOD_NAME,
                                                              exception);
         }
@@ -72,11 +76,12 @@ implements ParameterizedType {
         retrieveEnumValueIdentifierMethodNameAndClass();
     }
 
+    @SuppressWarnings("unchecked")
     private void retrieveEnumValueIdentifierMethodNameAndClass()
     throws InvalidUserTypeParameterValueException {
         try {
             final String enumClassName = getParameterValue(parameters, PARAMETERNAME_ENUM_CLASS_NAME);
-            enumClass = findClass(enumClassName, IdEnum.class);
+            enumClass = useClass(enumClassName).withType(IdEnum.class).assertSubtypeOf(Enum.class).getParametrized();
             enumValueIdentifierMethodName = getParameterValue(parameters, PARAMETERNAME_ENUM_VALUE_METHOD_NAME);
         }
         catch (final ClassInstanceException exception) {
